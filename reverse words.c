be03195004b46f2ab5ebac264fc317ceae111c60
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2019-12-20 19:53:28 +0530

    Update reverse words.c

--- reverse words.c
+++ reverse words.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 int main()
 {
 char s[1000];
-scanf("%[^n]c",&s);
+scanf("%[^\n]c",&s);
 int len=strlen(s);
-int l=len-1,v=0;
-for(int i=len-1;i<=0;i++){
+int l=len-1,v=0,g=0;
+for(int i=len-1;i>=0;i--){
 if(s[i]==' ')
 {
 s[i]='\0';
@@ -14,12 +15,13 @@ v=i+1;
 g=l;
 for(int j=v;j<=g;j++)
 {
-printf("%c",s[i]);
+printf("%c",s[j]);
 }
+printf(" ");
 l=v-2;
 v=0;
-g=0;}
-printf("\t");
+g=0;
+}
 }
 printf("%s",s);
 }
