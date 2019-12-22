91d0c7f8b17dee177c1375d510939b086cd6422e
sudhamurugan06 <50529864+sudhamurugan06@users.noreply.github.com>
2019-12-22 20:03:47 +0530

    Update [ZOHO] Rotate 90 deg MATRIX(anti clockwise).c

--- [ZOHO] Rotate 90 deg MATRIX(anti clockwise).c
+++ [ZOHO] Rotate 90 deg MATRIX(anti clockwise).c
@@ -3,7 +3,7 @@ int main()
 {
 int n,m;
 scanf("%d %d",&n,&m);
-int a[n][m],b[m][n];
+int a[n][m];
 for(int i=0;i<n;i++)
 {
 for(int j=0;j<m;j++)
@@ -11,21 +11,11 @@ for(int j=0;j<m;j++)
 scanf("%d",&a[i][j]);
 }
 }
-int s=m,ps=0,bt=0;
-for(int i=s;i>=0;i--)
+for(int i=m;i>=0;i--)
 {
 for(int j=0;j<n;j++)
 {
-b[ps][bt++]=a[j][i];
-}
-ps++;
-bt=0;
-}
-for(int i=1;i<=s;i++)
-{
-for(int j=0;j<n;j++)
-{
-printf("%d ",b[i][j]);
+printf("%d ",a[i][j]);
 }
 printf("\n");
 }
